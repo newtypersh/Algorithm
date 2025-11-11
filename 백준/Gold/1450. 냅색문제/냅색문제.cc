926b55313b155de3faedb4fac2fd25847846f421
newtypersh <147497131+newtypersh@users.noreply.github.com>
2025-11-12 08:31:19 +0900

    [Gold I] Title: 냅색문제, Time: 4 ms, Memory: 2680 KB -BaekjoonHub

---
+++ 백준/Gold/1450. 냅색문제/냅색문제.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+vector<ll> v;
+
+void dfs(int start, int end, vector<ll>& part, ll sum) {
+	if (start > end) {
+		part.push_back(sum);
+		return;
+	}
+	else {
+		dfs(start + 1, end, part, sum);
+		dfs(start + 1, end, part, sum + v[start]);
+	}
+}
+
+int main() {
+	int n, c;
+	cin >> n >> c;
+	v.resize(n, 0);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+	vector<ll> part1;
+	vector<ll> part2;
+	dfs(0, (n / 2) - 1, part1, 0);
+	dfs((n / 2), n - 1, part2, 0);
+	sort(part2.begin(), part2.end());
+	int cnt = 0;
+	for (int i = 0; i < part1.size(); i++) {
+		ll x = c - part1[i]; // x는 더 담을 수 있는 무게
+		cnt += upper_bound(part2.begin(), part2.end(), x) - part2.begin();
+		// part2 에서 x를 초과하는 위치의 인덱스
+	}
+	cout << cnt;
+	return 0;
+}
