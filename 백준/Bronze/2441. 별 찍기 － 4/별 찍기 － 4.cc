18d196d3ed9eb78a08668fe751cc5cd1d4852ad0
newtypersh <147497131+newtypersh@users.noreply.github.com>
2025-11-12 08:41:48 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = n; i > 0; i--) {
+		for (int j = n; j > 0; j--) {
+			if (i >= j) {
+				cout << "*";
+			}
+			else {
+				cout << " ";
+			}
+		}
+		cout << "\n";
+	}
+
+
+	return 0;
+}
