42bbf1bae294a1fac32f581a637652802e7dd2d7
newtypersh <147497131+newtypersh@users.noreply.github.com>
2025-11-11 17:03:19 +0900

    [Silver II] Title: 숫자판 점프, Time: 40 ms, Memory: 33948 KB -BaekjoonHub

---
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
@@ -0,0 +1,24 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+grid = [list(map(int, input().split())) for _ in range(5)]
+num_set = set()
+
+def dfs(i, j, num):
+    num += str(grid[i][j])
+
+    if len(num) == 6:
+        num_set.add(num)
+        return
+
+    for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+        x, y = i + dx, j + dy
+        if 0 <= x < 5 and 0 <= y < 5:
+            dfs(x, y, num)
+
+for i in range(5):
+    for j in range(5):
+        dfs(i, j, '')
+
+print(len(num_set))
