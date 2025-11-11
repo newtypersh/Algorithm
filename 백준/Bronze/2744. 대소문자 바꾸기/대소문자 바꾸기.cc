486ed4360691dd5c24406b70ff175bd808cfa9fd
newtypersh <147497131+newtypersh@users.noreply.github.com>
2025-11-12 08:50:07 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	string str;
+
+	cin >> str;
+
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] >= 65 && str[i] <= 90) {
+			str[i] += 32;
+		}
+		else if (str[i] >= 97 && str[i] <= 122) {
+			str[i] -= 32;
+		}
+	}
+
+	cout << str;
+
+	return 0;
+}
