84fdac5d70b1259d096a5ac8260d8341b6ad1c00
newtypersh <147497131+newtypersh@users.noreply.github.com>
2025-11-12 08:34:16 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2252 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <queue>
+#define MAX 100001
+using namespace std;
+
+int n, k;
+bool visited[MAX];
+
+void bfs(int a) {
+	queue<pair<int, int>> q;
+	q.push(make_pair(a, 0));
+	while (!q.empty()) {
+		int x = q.front().first;
+		int cnt = q.front().second;
+		q.pop();
+		if (x == k) {
+			cout << cnt;
+			break;
+		}
+		if (x + 1 >= 0 && x + 1 < MAX) {
+			if (!visited[x + 1]) {
+				visited[x + 1] = true;
+				q.push(make_pair(x + 1, cnt + 1));
+			}
+		}
+		if (x - 1 >= 0 && x - 1 < MAX) {
+			if (!visited[x - 1]) {
+				visited[x - 1] = true;
+				q.push(make_pair(x - 1, cnt + 1));
+			}
+		}
+		if (2 * x >= 0 && 2 * x < MAX) {
+			if (!visited[2 * x]) {
+				visited[2 * x] = true;
+				q.push(make_pair(2 * x, cnt + 1));
+			}
+		}
+	}
+	
+
+}
+
+int main() {
+	cin >> n >> k;
+	visited[n] = true;
+	bfs(n);
+	return 0;
+}
