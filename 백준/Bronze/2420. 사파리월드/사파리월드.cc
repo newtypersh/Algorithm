ca9d712c23dea9b8e7cedaeeba8156ae57a251b7
newtypersh <147497131+newtypersh@users.noreply.github.com>
2025-11-12 08:40:52 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+    
+    long long int a, b;
+    cin >> a >> b;
+    cout << abs(a - b);
+}
+
