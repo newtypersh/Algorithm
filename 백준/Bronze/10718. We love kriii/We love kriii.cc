d2eab362d1267ce5e383b24d04e7ad1f7026be16
newtypersh <147497131+newtypersh@users.noreply.github.com>
2025-11-12 11:50:24 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	
+	cout << "강한친구 대한육군" << endl;
+	cout << "강한친구 대한육군" << endl;
+
+	return 0;
+}
+
+//5+5 = 10, 50 5+0 = 5, 5
