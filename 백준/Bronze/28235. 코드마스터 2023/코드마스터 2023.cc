38425e71feb75b21ac2d658570c90ed42f268ac9
newtypersh <147497131+newtypersh@users.noreply.github.com>
2025-11-12 12:28:44 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+	if (str == "SONGDO") {
+		cout << "HIGHSCHOOL";
+	}
+	if (str == "CODE") {
+		cout << "MASTER";
+	}
+	if (str == "2023") {
+		cout << "0611";
+	}
+	if (str == "ALGORITHM") {
+		cout << "CONTEST";
+	}
+
+}
