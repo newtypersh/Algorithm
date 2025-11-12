68b73cbf38907281c9f5bfed165305f0d5b7c1ab
newtypersh <147497131+newtypersh@users.noreply.github.com>
2025-11-12 09:07:10 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.03 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static int gcd(int a, int b) {
+        if(b == 0) return a;
+        return gcd(b, a % b);
+    }
+    
+    public static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+    
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        if(n > m) {
+            answer[0] = gcd(n, m);
+            answer[1] = lcm(n, m);
+        }
+        else {
+            answer[0] = gcd(m, n);
+            answer[1] = lcm(m, n);
+        }
+        
+        
+        return answer;
+    }
+}
