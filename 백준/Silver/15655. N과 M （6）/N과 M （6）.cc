0fe7d5edfa082a014f76a6693a3a7021b1effc52
newtypersh <147497131+newtypersh@users.noreply.github.com>
2025-11-12 12:24:30 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int arr[8];
+int num[8];
+int check[8];
+
+void seq(int idx, int len) {
+	if (len == M) {
+		for (int i = 0; i < M; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = idx; i < N; i++) {
+		if (check[i] == 0) {
+			arr[len] = num[i];
+			check[i] = true;
+			seq(i + 1, len + 1);
+			check[i] = false;
+		}
+	}
+}
+
+int main() {
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		cin >> num[i];
+	}
+
+	sort(num, num+N);
+
+	seq(0, 0);
+
+
+	return 0;
+}
