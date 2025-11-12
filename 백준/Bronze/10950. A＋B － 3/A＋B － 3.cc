f179043549842b4c7d9908229eeb0984b882d106
newtypersh <147497131+newtypersh@users.noreply.github.com>
2025-11-12 11:55:39 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b, num;
+    cin >> num;
+    for(int i=0;i<num;i++){
+        cin >> a >> b;
+        cout << a+b;
+        cout << endl;
+    }
+    
+    
+}
