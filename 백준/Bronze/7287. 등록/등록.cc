0b122e33088c033ed0fff8ae8be9888e28fc8a8b
newtypersh <147497131+newtypersh@users.noreply.github.com>
2025-11-12 11:38:37 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	cout << 100 << endl;
+	cout << "allthat1000";
+
+	return 0;
+}
+
