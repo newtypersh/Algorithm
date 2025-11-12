527e35a882283d31428f7ff335db68e1d24e35ea
newtypersh <147497131+newtypersh@users.noreply.github.com>
2025-11-12 11:55:20 +0900

    [Bronze III] Title: 팩토리얼, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int num, total = 1;
+	cin >> num;
+	for (int i = 1; i <= num; i++) {
+		total *= i;
+	}
+	cout << total;
+
+	return 0;
+}
+
