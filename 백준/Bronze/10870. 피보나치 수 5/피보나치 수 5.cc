6f54cf35aeac7a2341a559441a306e45d867c396
newtypersh <147497131+newtypersh@users.noreply.github.com>
2025-11-12 11:54:58 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+int Fibonacci(int n) {
+	if (n <= 1) {
+		return n;
+	}
+	else {
+		return Fibonacci(n - 1) + Fibonacci(n - 2);
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+	cout << Fibonacci(n) << endl;
+}
