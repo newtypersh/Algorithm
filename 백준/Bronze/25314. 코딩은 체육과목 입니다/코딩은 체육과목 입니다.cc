800a7c6057c12d61231f338ebc7201878350a781
newtypersh <147497131+newtypersh@users.noreply.github.com>
2025-11-12 12:28:17 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num, print;
+	cin >> num;
+	print = num/4;
+	for (int i = 0; i < print;i++) {
+		cout << "long ";
+	}
+	cout << "int";
+}
