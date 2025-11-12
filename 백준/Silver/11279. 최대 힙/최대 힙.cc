b37672fa25a387f35b0c7d80ac8b40f92c91969c
newtypersh <147497131+newtypersh@users.noreply.github.com>
2025-11-12 11:58:26 +0900

    [Silver II] Title: 최대 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cout.tie(0);
+    cin.tie(0);
+
+    priority_queue<int> pq;
+    int n, x;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        if (x != 0) {
+            pq.push(x);
+        }
+        else {
+            if (pq.empty()) {
+                cout << 0 << '\n';
+            }
+            else {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        }
+    }
+    
+    
+    return 0;
+}
