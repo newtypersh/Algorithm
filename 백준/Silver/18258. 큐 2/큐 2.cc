2ee98fced142b3533948213e6e1b176836834913
newtypersh <147497131+newtypersh@users.noreply.github.com>
2025-11-12 12:27:16 +0900

    [Silver IV] Title: 큐 2, Time: 384 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cout.tie(0);
+    cin.tie(0);
+
+    queue<int> q;
+    string str;
+    int n, x;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> str;
+        if (str == "push") {
+            cin >> x;
+            q.push(x);
+        }
+        else if (str == "pop") {
+            if (q.empty()) {
+                cout << -1 << '\n';
+            }
+            else {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+        }
+        else if (str == "size") {
+            cout << q.size() << '\n';
+        }
+        else if (str == "empty") {
+            if (q.empty()) {
+                cout << 1 << '\n';
+            }
+            else {
+                cout << 0 << '\n';
+            }
+        }
+        else if (str == "front") {
+            if (q.empty()) {
+                cout << -1 << '\n';
+            }
+            else {
+                cout << q.front() << '\n';
+            }
+        }
+        else if (str == "back") {
+            if (q.empty()) {
+                cout << -1 << '\n';
+            }
+            else {
+                cout << q.back() << '\n';
+            }
+        }
+    }
+    
+    
+    return 0;
+}
